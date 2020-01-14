8b323fbc2d5098a10136f255b57288d7ab3d0b5e
Nicolas Mariano Obregon <nmobregon@thinkup.com.ar>
2020-01-14 16:53:53 -0300

    Updates

---
+++ precache-manifest.356fb3596fb39c13b45bcaee7d5e3fe7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0db1961ec7796d1cf469cef401ce50b",
+    "url": "/devsargentinos/index.html"
+  },
+  {
+    "revision": "7bbf9089aa38de412727",
+    "url": "/devsargentinos/static/css/main.687c71c8.chunk.css"
+  },
+  {
+    "revision": "ca4dfa9661cb9359823e",
+    "url": "/devsargentinos/static/js/2.6252f0b3.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/devsargentinos/static/js/2.6252f0b3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7bbf9089aa38de412727",
+    "url": "/devsargentinos/static/js/main.b47ab683.chunk.js"
+  },
+  {
+    "revision": "d0eccbd1ade9cf91f54d",
+    "url": "/devsargentinos/static/js/runtime-main.fb2ba8ce.js"
+  }
+]);
